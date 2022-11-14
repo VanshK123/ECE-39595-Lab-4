958313defdcec81f782f1a17f987190d8d06b266
Vansh Khanna <khanna57@ecegrid-thin1.ecn.purdue.edu>
2022-11-14 15:15:50 -0500

    initalized .cc

---
+++ poly.cc
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <chrono>
+#include <optional>
+#include <vector>
+
+#include "poly.h"
+
+polynomial::polynomial()
+{
+    // TODO
+}
+
+template <typename Iter>
+polynomial::polynomial(Iter begin, Iter end)
+{
+    // TODO
+}
+
+polynomial::polynomial(const polynomial &other)
+{
+
+}
+
+void polynomial::print() const
+{
+
+}
+polynomial::polynomial &operator=(const polynomial &other)
+{
+
+}
+polynomial::polynomial operator+(const polynomial &other) const
+{
+
+}
+polynomial::polynomial operator*(const polynomial &other) const
+{
+
+}
+bool polynomial::operator<(const polynomial &other) const
+{
+
+}
+
+bool polynomial::operator>(const polynomial &other) const
+{
+
+}
+bool polynomial::operator==(const polynomial &other) const
+{
+
+}
